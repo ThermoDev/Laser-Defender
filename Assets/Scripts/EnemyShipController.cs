fa6fb7c8ee655daa344177449bf134c2b77b9672
ThermoLink <newbleter@gmail.com>
2017-04-10 00:29:30 +1000

    LD09: Shooting Enemies

---
+++ Assets/Scripts/EnemyShipController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShipController : MonoBehaviour {
+	private Projectile projectile;
+	public float health = 100f;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		projectile = collider.gameObject.GetComponent<Projectile>();
+		if(projectile != null){
+			health -= projectile.GetDamage();
+			projectile.Hit();
+
+			if(health <= 0){
+				Destroy(gameObject);
+			}
+			print ("Woot");
+		}
+	}
+}
